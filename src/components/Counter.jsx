031d21bab59cc9cc5a1f6e96524e8d087c258e47
diegogirao <diegoramiro25@gmail.com>
2023-04-03 19:35:27 +0100

    Style change text position and bg color

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,11 +3,11 @@ import React from "react"
 const Counter = ({ title, number }) => {
 	return (
 		<div className="grid grid-flow-col gap-5 text-center auto-cols-max">
-			<div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+			<div className="flex flex-col p-2 bg-neutral-800 rounded-box text-neutral-content">
 				<div className="countdown font-mono text-5xl">
 					<p style={{ "--value": number }}></p>
 				</div>
-				<h3 className="countdown">{title}</h3>
+				<h3 className="countdown justify-center">{title}</h3>
 			</div>
 		</div>
 	)

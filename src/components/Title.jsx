3fc54af4db940a9d6af49a8fc4903533ceb6e3f8
diegogirao <diegoramiro25@gmail.com>
2023-04-03 19:36:06 +0100

    Style change style of the title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,11 @@
 import React from "react"
 
 const Title = ({ title }) => {
-	return <h1 className="card-title">{title}</h1>
+	return (
+		<h1 className="mb-5 text-5xl font-bold text-white drop-shadow-lg">
+			{title}
+		</h1>
+	)
 }
 
 export default Title

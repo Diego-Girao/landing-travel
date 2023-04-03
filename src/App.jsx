90759156cb830f4960258e3f71782cac3f548585
diegogirao <diegoramiro25@gmail.com>
2023-04-03 19:33:51 +0100

    Style changed card to hero and adjusted width and height

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,27 @@ import useCountdown from "./hooks/useCountdown"
 import "./index.css"
 
 function App() {
-	const [day, hour, minute, second] = useCountdown("Mar 27, 2023 10:30:00")
+	const [day, hour, minute, second] = useCountdown("Apr 10, 2023 10:30:00")
 
 	return (
 		<div className="container mx-auto">
-			<div className="card bg-base-100 shadow-xl">
+			<div className="hero bg-base-100 shadow-xl">
 				<figure>
-					<img src={bgPt} alt="lisbon raiwls" />
+					<img
+						src={bgPt}
+						className="max-h-screen w-screen"
+						alt="lisbon raiwls"
+					/>
 				</figure>
-				<div>
-					<div className="card-bod flex flex-row gap-5">
-						<Title title="Chegaremos em Portugal 🇵🇹" />
-						<Counter title="Dias" number={day} />
-						<Counter title="Horas" number={hour} />
-						<Counter title="Minutos" number={minute} />
-						<Counter title="Segundos" number={second} />
-					</div>
+				<div className="hero-overlay bg-opacity-60"></div>
+				<div className="-mt-40">
+					<Title title="Chegada em Portugal 🇵🇹" />
+				</div>
+				<div className="max-w-md flex flex-row gap-5">
+					<Counter title="Dias" number={day} />
+					<Counter title="Horas" number={hour} />
+					<Counter title="Minutos" number={minute} />
+					<Counter title="Segundos" number={second} />
 				</div>
 			</div>
 		</div>
